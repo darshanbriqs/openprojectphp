d2132175e38f7adcef953139576889550b6ad52b
Darshan Sanandiya <darshan.sanandiya@qdata.io>
2017-11-30 10:20:16 +0530

    changes in task and project services

--- src/Service/ProjectService.php
+++ src/Service/ProjectService.php
@@ -8,21 +8,21 @@ class ProjectService extends AbstractService
      * Returns all projects
      */
     public function all( $options = array() ) {
-        return $this->client->request( '/api/v3/projects', 'get', $options );   
+        return $this->client->request( 'api/v3/projects', 'get', $options );   
     }
     
     /**
     *  Returns one project  
     */
     public function one( $project_id, $options = array() ) {
-        return $this->client->request( '/api/v3/project/'.$project_id.'', 'get', $options );   
+        return $this->client->request( 'api/v3/project/'.$project_id.'', 'get', $options );   
     }
     
     /**
     *  Create Project  
     */
     public function create( $options = array() ) {
-        return $this->client->request( '/api/v3/projects', 'post', $options );
+        return $this->client->request( 'api/v3/projects', 'post', $options );
     }
     
    
--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -11,14 +11,14 @@ class TaskService extends AbstractService
         $options = array();
         $options['type_id']['operator'] = '=';
         $options['type_id']['values'] = 1;
-        return $this->client->request( '/api/v3/projects/'.$project_id.'/work_packages', 'get', $options );   
+        return $this->client->request( 'api/v3/projects/'.$project_id.'/work_packages', 'get', $options );   
     }
     
     /**
     *  Returns one task  
     */
     public function one( $task_id, $options = array() ) {
-        return $this->client->request( '/api/v3/work_packages/'.$task_id.'', 'get', $options );   
+        return $this->client->request( 'api/v3/work_packages/'.$task_id.'', 'get', $options );   
     }
     
     /**
@@ -31,7 +31,7 @@ class TaskService extends AbstractService
         $options['_links'] =  array(
           "type" => array("href"=>"/api/v3/types/1")
         );
-        return $this->client->request( '/api/v3/projects/'.$project_id.'/work_packages', 'post', $options );
+        return $this->client->request( 'api/v3/projects/'.$project_id.'/work_packages', 'post', $options );
     }
     
     /**
@@ -49,6 +49,6 @@ class TaskService extends AbstractService
             $options['description'] = array('format'=>'textile','raw'=>$data['description']);
         }
         
-        return $this->client->request( '/api/v3/work_packages/'.$task_id.'', 'patch', $options );
+        return $this->client->request( 'api/v3/work_packages/'.$task_id.'', 'patch', $options );
     }
 }
